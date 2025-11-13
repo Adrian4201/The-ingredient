f9ce498095b02024adca6704c26ca2805e81a977
Adrian4201 <cookar6@miamioh.edu>
2025-11-13 11:02:13 -0500

    i reilezed i there is a shuffle eck effect

---
+++ The ingrident/Assets/RogueEngine/Scripts/Conditions/ConditionShuffleDeck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RogueEngine.AI;
+
+namespace RogueEngine
+{
+    /// <summary>
+    /// Condition that compares the target category of an ability to the actual target (card, player or slot)
+    /// </summary>
+
+    [CreateAssetMenu(fileName = "condition", menuName = "TcgEngine/Condition/ShuffleDeck", order = 10)]
+    public class ConditionShuffleDeck : ConditionData
+    {
+        
+        public override bool IsTargetConditionMet(Battle data, AbilityData ability, BattleCharacter character, Card card, CardData target)
+        {
+            return base.IsTargetConditionMet(data, ability, character, card, target);
+        }
+    }
+    
+}
