f616e13aa3fdaec03729e647fd96e346ed21b1bb
Adrian4201 <cookar6@miamioh.edu>
2025-12-04 07:58:54 -0500

    levelloader

---
+++ The ingrident/Assets/RogueEngine/Scripts/Levelloader.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Levelloader : MonoBehaviour
+{
+    public Animator animator;
+    public float Transtiontime = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            loadNextlevel();
+        }
+        
+    }
+    public void loadNextlevel()
+    {
+        StartCoroutine(Loadlevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator Loadlevel(int level)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(Transtiontime);
+
+        SceneManager.LoadScene(level);
+    }
+}
