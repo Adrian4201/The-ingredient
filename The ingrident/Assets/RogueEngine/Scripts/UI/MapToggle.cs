355fddb09b9985d34b52be8345b1cf63b2fd2351
Adrian4201 <cookar6@miamioh.edu>
2026-02-24 18:21:39 -0500

    reconvine at latter date

---
+++ The ingrident/Assets/RogueEngine/Scripts/UI/MapToggle.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MapToggle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("UI button")]
+    public TextMeshProUGUI button;
+
+   public void OnButtonClick()
+    {
+
+    }
+}
